ae3a32186564d4a9ff83aeff4a8a2e7d062efee1
BulSV <SB.ua@yandex.ru>
2015-03-25 10:14:40 +0200

    Fixed curves tracking

--- PlotterDialog.cpp
+++ PlotterDialog.cpp
@@ -328,11 +328,11 @@ void PlotterDialog::autoScroll(const double &elapsedTime)
 
     double tempFactor = dynamic_cast<QLCDNumber *>( m_lcdTempInterval->spinWidget() )->value();
 
-    if( m_dataAxises.at(0).last() > m_prevTempOffset + tempFactor * YDIVISION / 2 ) {
+    if( m_dataAxises.at(0).last() > m_prevCentralTemp + tempFactor * YDIVISION / 2 ) {
         m_msbTempInterval->upStep();
     }
 
-    if( m_dataAxises.at(0).last() < m_prevTempOffset - tempFactor * YDIVISION / 2 ) {
+    if( m_dataAxises.at(0).last() < m_prevCentralTemp - tempFactor * YDIVISION / 2 ) {
         m_msbTempInterval->downStep();
     }
 }
